3520b499362b761771bba3b9f3513ffefc70a16b
Kushagra Varshney <devkussu43@gmail.com>
2025-08-04 13:21:44 +0530

    hashrouter in app.js

--- expense-tracker-frontend/src/App.jsx
+++ expense-tracker-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { HashRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
