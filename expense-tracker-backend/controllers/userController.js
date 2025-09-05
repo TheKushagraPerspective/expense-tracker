bd9226303844854071f2645bde9e3f500ca6586a
Kushagra Varshney <devkussu43@gmail.com>
2025-09-05 19:19:50 +0530

    otp

--- expense-tracker-backend/controllers/userController.js
+++ expense-tracker-backend/controllers/userController.js
@@ -125,7 +125,10 @@ const getOTP = async (req, res) => {
 
 
         // Store OTP with 5 minutes expiry
+        console.log("Attempting to store OTP in Redis..."); // <--- Add this line
         await client.set(`otp:${email}`, otpSixDigit, { ex: 300 }); // ex is in seconds
+        console.log("OTP successfully stored in Redis."); // <--- Add this line
+
 
 
         return res.status(200).json({
